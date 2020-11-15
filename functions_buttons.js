f93fea9d723dedaf19f2160ca4d09ebb2c49f69a
OverAhah <40232911+OverAhah@users.noreply.github.com>
2020-11-15 12:31:58 +0300

    Add files via upload

---
+++ functions_buttons.js
@@ -0,0 +1,39 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key == "a") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/A.mp3");
+        audio.play();
+    } else if (event.key == "s") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/S.mp3");
+        audio.play();
+    } else if (event.key == "d") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/D.mp3");
+        audio.play();
+    } else if (event.key == "f") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/F.mp3");
+        audio.play();
+    }else if (event.key == "g") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/G.mp3");
+        audio.play();
+    }else if (event.key == "h") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/H.mp3");
+        audio.play();
+    }else if (event.key == "j") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records/J.mp3");
+        audio.play();
+    }else if (event.key == "w") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records-Black/W.mp3");
+        audio.play();
+    } else if (event.key == "e") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records-Black/E.mp3");
+        audio.play();
+    } else if (event.key == "t") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records-Black/T.mp3");
+        audio.play();
+    }else if (event.key == "y") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records-Black/Y.mp3");
+        audio.play();
+    }else if (event.key == "u") {
+        let audio = new Audio("C:/Users/kozak/Desktop/Records-Black/U.mp3");
+        audio.play();
+    }
+});
